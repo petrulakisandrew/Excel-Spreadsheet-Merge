14fbb1f3b32a82ecedae4c2530767882f72e40ca
Andrew Petrulakis <152444075+petrulakisandrew@users.noreply.github.com>
2023-12-05 16:27:59 -0600

    Add files via upload

---
+++ SpreadsheetMergeGit.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd 
+
+
+#####  Functions #####
+def dataset(name, directory, sheetname):
+    for file in os.listdir(directory):
+        if file.endswith('.xlsx'):
+            print('Loading file {0}...'.format(file))
+            name.append(pd.read_excel(os.path.join(directory, file), sheet_name=sheetname))
+
+def createsheet(data, name):
+    temp_master = pd.concat(data, axis=0)
+    temp_master.to_excel(name, index = False)
+ 
+ 
+ 
+ 
+    
+#####  Example  #####
+data_file_folder1 = 'C:/Users/Andrew Petrulakis/Desktop/Reports'
+
+df1 = []
+dataset(df1, directory = 'C:/Users/Andrew Petrulakis/Desktop/Reports/SEMAP/SEMAP 2/KHA', sheetname = 'Merge')
+        
+# print(df1)
+#print(len(df1))
+
+createsheet(df1, name = 'Final')
